6cb6f3afb90a152e81fac330ce3b09ad6557aed2
Paulo Rezende <pmbr@pmbr.net>
2017-06-22 07:48:25 -0700

    Adding to README

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## FileIndexer is an example of Hadoop MapReduce implementation
 
-It shows how to read files from HDFS, process them, and save result to HDFS
+It shows how to read files from HDFS, process them, and save result to HDFS.
+
+Input files are any plain text files.
+
+Output file is a file containing an index of all words found on input file and the list of files where each word was found.
--- src/main/java/net/pmbr/hadoop/fileindexer/FileMapper.java
+++ src/main/java/net/pmbr/hadoop/fileindexer/FileMapper.java
@@ -10,21 +10,17 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class FileMapper extends Mapper<LongWritable, Text, Text, Text> {
 
-	private Text filename;
+	private Text filename = new Text();
 	private Text word = new Text();
 
 	@Override
 	protected void setup(Context context) throws IOException, InterruptedException {
-
 		FileSplit fileSplit = ((FileSplit) context.getInputSplit());
-
-		this.filename = new Text(fileSplit.getPath().getName());
-
+		this.filename.set(fileSplit.getPath().getName());
 	}
 
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
 		for (String token : StringUtils.split(value.toString())) {
 			word.set(token);
 			context.write(word, filename);
